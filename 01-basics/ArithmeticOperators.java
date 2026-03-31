0c2dd285b40b1dd7c97ecef88e6e71e8e54ef9cb
Fanuel <siruonyango77@gmail.com>
2026-03-31 22:44:59 +0300

    Basic Arithmetic Operators

---
+++ 01-basics/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+        int x = 10;
+        int y = 3;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        z = x % y;
+
+        System.out.println(z);
+
+    }
+}
