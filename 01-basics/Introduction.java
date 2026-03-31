56d19ce3d46e5c68c695c5e4057082f864df87dc
Fanuel SiRU <144348586+FSiRU@users.noreply.github.com>
2026-03-31 17:50:22 +0300

    Add Introduction.java with basic Java program

---
+++ 01-basics/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Welcome to my first Java Program.");
+        System.out.println("This is going to be fun.");
+        
+        System.out.print("Hello World!\n");
+        System.out.print("Welcome to my first Java Program.\n");
+        System.out.print("This is going to be fun.\n");
+    }
+}
