e50797aeadf2dc4d6e051f0b28daf120b1ca4bd8
Fanuel <siruonyango77@gmail.com>
2026-04-07 14:25:33 +0300

    Add examples

--- Learning_structure/control_flow_02/TernaryOperator.java
+++ Learning_structure/control_flow_02/TernaryOperator.java
@@ -13,22 +13,33 @@ public class TernaryOperator {
         // PASS or FAIL
         int score = 40;
         String passOrFail = (score >= 50) ? "PASS" : "FAIL";
-        System.out.println(passOrFail);
+        System.out.println("\n" + passOrFail);
 
         // ODD or EVEN
         int number = 4;
         String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
-        System.out.println(evenOrOdd);
+        System.out.println("\n" + evenOrOdd);
 
         // Time of DAY
         int hours = 13;
         String timeOfDay = (hours < 12) ? "A.M" : "P.M";
-        System.out.println(timeOfDay);
+        System.out.println("\n" + timeOfDay);
 
         // Tax Rate
         int income = 60000;
         double taxRate = (income >= 40000) ? 0.25 : 0.15;
-        System.out.println(taxRate);
+        System.out.println("\n" + taxRate);
 
+        // Max of two numbers
+        int a = 15;
+        int b = 27;
+        int max = (a > b) ? a : b;
+        System.out.println("\nMax of " + a + " and " + b + " is: " + max);
+
+        // Discount eligibility
+        boolean isMember = true;
+        double discount = isMember ? 0.20 : 0.05;
+        System.out.println("\nMember: " + isMember);
+        System.out.println("Discount: " + (discount * 100) + "%");
     }
 }
